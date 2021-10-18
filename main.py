ca16b8b8cde5539ff34f9d0f8ff506b6b21b1915
CodingClub-KCHigh <78730740+CodingClub-KCHigh@users.noreply.github.com>
2021-10-18 11:53:06 +0530

    Finish task 2

--- main.py
+++ main.py
@@ -18,3 +18,24 @@ for name in names:
 print(f"The number of holes to be played is {num_holes}")
 print(f"The par for the course is {par}")
 # ---------------------------------------------
+
+# ------------- TASK 2 ------------------
+num_holes_played = 1
+while num_holes >= 0:
+    print(f"\n{num_holes_played} hole(s) has been played")
+    for name in names:
+        stroke_1 = int(input(f"{name} enter the number of strokes you played: "))
+        stroke_2 = int(input("Enter the number of strokes again to confirm: "))
+        while stroke_1 != stroke_2:
+            print("Error. Both strokes are not equal.")
+            stroke_1 = int(input(f"{name} enter the number of strokes: "))
+            stroke_2 = int(input(f"{name} enter the number of strokes again to confirm: "))
+        scores[names.index(name)] += stroke_2
+        choice = input("Do you wish to see the total number of strokes you have played? (y/n): ")
+        if choice == "y":
+            print(f"{name}'s total strokes is {scores[names.index(name)]}")
+        print("Strokes entered successfully")
+    print(f"You have {num_holes-1} holes left to play")
+    num_holes-=1
+    num_holes_played+=1
+# ---------------------------------------------
