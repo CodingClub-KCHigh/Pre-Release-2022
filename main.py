42a0c0a739abff8ccac8c42d81bf69eda9cccc96
CodingClub-KCHigh <78730740+CodingClub-KCHigh@users.noreply.github.com>
2021-10-20 09:51:23 +0530

    Finish half of task 3

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+from array import *
 # ------------- TASK 1 ------------------
 num_players = int(input("Enter the number of players taking part: "))
 while num_players > 4 or num_players < 2:
     num_players = int(input("Error. Only 2, 3 or 4 people play at one given time. Re enter the number of players playing: "))
 num_holes = int(input("Enter the number of holes to be played (9/18): "))
 names = []
-scores = [0]*num_players
+scores = [0] * num_players
 for count in range(0, num_players):
     name = input(f"Enter player {count+1}'s name': ")
     names.append(name)
@@ -21,7 +22,7 @@ print(f"The par for the course is {par}")
 
 # ------------- TASK 2 ------------------
 num_holes_played = 1
-while num_holes >= 0:
+while num_holes > 0:
     print(f"\n{num_holes_played} hole(s) has been played")
     for name in names:
         stroke_1 = int(input(f"{name} enter the number of strokes you played: "))
@@ -38,4 +39,16 @@ while num_holes >= 0:
     print(f"You have {num_holes-1} holes left to play")
     num_holes-=1
     num_holes_played+=1
+print("The round is over")
 # ---------------------------------------------
+# ------------- TASK 3 ------------------
+#!!! Task 3 is not fully completed
+for name in names:
+    score_par = scores[names.index(name)] - par
+    if score_par > 0:    
+        print(f"{name}'s score is {scores[names.index(name)]}. Their score relative to par is {score_par} over par")
+    elif score_par < 0:
+        print(f"{name}'s score is {scores[names.index(name)]}. Their score relative to par is {abs(score_par)} under par")
+    else:
+        print(f"{name}'s score is {scores[names.index(name)]}. Their score relative is equal to the par")
+print(f"The winner is {names[scores.index(min(scores))]}. Their score is {min(scores)}")  
